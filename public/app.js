372454379b2f5551a9f87654d118bbd95fd30b32
Clément Bouillier <clement.bouillier@devcrafting.com>
2016-02-01 22:34:52 +0100

    Loading message while waiting for synthesis data

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
         app.controller("ActivitiesByMonthViewModel", function($scope, $http) {    
             var self = this;
             
-            self.text = "";
+            self.loading = true;
             self.months = [];
             self.activities = [];
             self.totals = [];
@@ -13,10 +13,12 @@
             self.totalRevenues = "-";
            
             var load = function() { 
+                self.loading = true;
                 $http.get('/synthesis').success(function(data) {
                     self.activities = data.activitiesByMonth;
                     self.totals = data.totals;
                     self.months = data.months;
+                    self.loading = false;
                 }); 
             };
             
